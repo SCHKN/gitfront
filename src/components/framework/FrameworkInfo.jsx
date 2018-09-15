dd0a9a47461af74bf4deeef33dbe507834ab8615
Antoine <solnichkin.antoine@gmail.com>
2018-09-15 20:26:00 +0200

    some cool animations when hovering a framework card

--- src/components/framework/FrameworkInfo.jsx
+++ src/components/framework/FrameworkInfo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Segment, Icon, Label } from "semantic-ui-react";
 import { fetchPosts } from "../../redux/repoReducers";
 import { connect } from "react-redux";
@@ -7,24 +7,37 @@ const mapDispatchToProps = dispatch => ({
   fetchPosts: framework => dispatch(fetchPosts(framework))
 });
 
-const FrameworkInfo = ({ fetchPosts, data, rank }) => {
-  const { framework } = data;
-  return (
-    <Segment
-      onClick={() => fetchPosts(framework)}
-      textAlign="center"
-      className="framework-info"
-    >
-      <Label attached="top" className={framework + "-top-label"}>
-        {framework}
-      </Label>
-      <Icon name={framework} size="massive" className="framework-icon" />
-      <Label attached="bottom">
-        <Icon name="trophy" className={"trophy-" + rank} /> Rank # {rank}
-      </Label>
-    </Segment>
-  );
-};
+class FrameworkInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isHovered: false };
+  }
+
+  render() {
+    const { fetchPosts, data, rank } = this.props;
+    const { framework } = data;
+    return (
+      <Segment
+        onClick={() => fetchPosts(framework)}
+        textAlign="center"
+        className="framework-info"
+        onMouseEnter={() => this.setState({ isHovered: true })}
+        onMouseLeave={() => this.setState({ isHovered: false })}
+      >
+        <Label attached="top" className={framework + "-label"}>
+          {framework}
+        </Label>
+        <Icon name={framework} size="massive" className="framework-icon" />
+        <Label
+          attached="bottom"
+          className={this.state.isHovered ? framework + "-label" : undefined}
+        >
+          <Icon name="trophy" className={"trophy-" + rank} /> Rank # {rank}
+        </Label>
+      </Segment>
+    );
+  }
+}
 
 export default connect(
   null,
