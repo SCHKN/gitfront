1e52d33372b58ddaef7e3f1f993b06c0bed418ca
Antoine <solnichkin.antoine@gmail.com>
2018-09-19 21:07:44 +0200

    updated the features message and reenabled GA

--- src/components/message/FeaturesMessage.jsx
+++ src/components/message/FeaturesMessage.jsx
@@ -3,10 +3,10 @@ import { Message } from "semantic-ui-react";
 const FeaturesMessage = () => {
   return (
     <Message
-      icon="cubes"
+      icon="reddit orange"
       header="Have you heard about the new features?"
       list={[
-        'Nuxt and PrimeNG frameworks have been added to Gitfront - check them out!'
+        'Reddit is now available as a datasource! Try it in the filter menu and fetch the best posts for your favorite framework.'
       ]}
     />
   );
